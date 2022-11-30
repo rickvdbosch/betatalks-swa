1f46d6d24b66fe2eaa15cfd59de9ef5f05ab6e69
Rick van den Bosch <rickvdbosch@outlook.com>
2022-11-30 11:49:49 +0100

    Migrate GetVideos to Azure.Data.Tables

--- Betatalks.SWA.API/GetVideos.cs
+++ Betatalks.SWA.API/GetVideos.cs
@@ -1,9 +1,9 @@
-using System.Collections.Generic;
 using System.Threading.Tasks;
+
+using Azure.Data.Tables;
 
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.Cosmos.Table;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
@@ -17,17 +17,9 @@ namespace Betatalks.SWA.API
         [FunctionName(nameof(GetVideos))]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "videos/{partitionKey}")] HttpRequest req,
-            [Table("Videos", Connection = "StorageConnectionString")] CloudTable cloudTable, string partitionKey, ILogger log)
+            [Table("Videos", Connection = "StorageConnectionString")] TableClient tableClient, string partitionKey, ILogger log)
         {
-            var videos = new List<VideoEntity>();
-            TableQuerySegment<VideoEntity> querySegment = null;
-            var query = new TableQuery<VideoEntity>().Where(TableQuery.GenerateFilterCondition(nameof(ITableEntity.PartitionKey), QueryComparisons.Equal, partitionKey));
-
-            do
-            {
-                querySegment = await cloudTable.ExecuteQuerySegmentedAsync(query, querySegment?.ContinuationToken);
-                videos.AddRange(querySegment.Results);
-            } while (querySegment.ContinuationToken != null);
+            var videos = tableClient.Query<VideoEntity>();
 
             return new OkObjectResult(videos);
         }
