f70558c97ca5e50f4a80a0e4cc8285ea17428e0f
Rick van den Bosch <rickvdbosch@outlook.com>
2022-11-30 11:49:18 +0100

    Migrate VideoEntity to Azure.Data.Tables

--- Betatalks.SWA.API/Entities/VideoEntity.cs
+++ Betatalks.SWA.API/Entities/VideoEntity.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.Azure.Cosmos.Table;
+﻿using System;
+using System.Runtime.Serialization;
+
+using Azure;
+using Azure.Data.Tables;
 
 namespace Betatalks.SWA.API.Entities
 {
-    public class VideoEntity : TableEntity
+    public class VideoEntity : ITableEntity
     {
-        [IgnoreProperty]
+        [IgnoreDataMember]
         public string Name
         { 
             get => RowKey;
@@ -16,5 +20,14 @@ namespace Betatalks.SWA.API.Entities
         public string Description { get; set; }
 
         public string Url { get; set; }
+
+        #region ITableEntity properties
+        
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+
+        #endregion
     }
 }
